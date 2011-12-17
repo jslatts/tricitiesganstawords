6366a5c0394df4aa052de5ad01c373599dd93acb
Beau Sorensen <mail@beausorensen.com>
2011-12-17 12:51:49 -0800

    Backgrounds and bootstrap

--- public/js/socket.js
+++ public/js/socket.js
@@ -8,6 +8,7 @@
     , socket = io.connect()
     , hud
     , usedWords
+    , players
   
   root.attackWords = {}
   root.usedWords = {}
@@ -22,6 +23,7 @@
 
     usedWords = $('#used-words')
     hud = $('#hud')
+    players = $('#players')
 
     function handleUsed(str, id) {
       root.usedWords[str] = id
@@ -31,17 +33,32 @@
     socket.on('connect', function() {
       root.playerId = socket.socket.sessionid
 
+      socket.on('used', function(words) {
+        if (!words) return
+
+        Object.keys(words).forEach(function(str) {
+          usedWords.prepend('<li>'+str+'</li>')
+        })
+      })
+
+      socket.on('players', function(people) {
+        if (!people) return
+
+        Object.keys(people).forEach(function(person) {
+          players.prepend('<li>'+person+'</li>')
+        })
+      })
 
       socket.on('attack', function(str, id) {
-        console.log('server attack')
-
+        str = str.toLowerCase().trim()
+        console.log('server attack', str, id)
         handleUsed(str, id)
         root.exports.incomingWord(str, (id === root.playerId))
       })
 
       socket.on('block', function(str, id) {
-        console.log('server block')
-        
+        str = str.toLowerCase().trim()
+        console.log('server block', str, id)
         root.exports.destroyWord(str, (id === root.playerId))
       })
 
@@ -61,6 +78,8 @@
     // ---------
 
     function attack(str, fn) {
+      str = str.toLowerCase().trim()
+      
       console.log('attack', str)
       socket.emit('attack', str, function(err) {
         console.log('return: ', err)
