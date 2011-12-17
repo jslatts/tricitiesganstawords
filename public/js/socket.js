cd34807cc9649623250e3a4446f0895e8c3d0f2b
Beau Sorensen <mail@beausorensen.com>
2011-12-17 11:52:06 -0800

    SVG overlay

--- public/js/socket.js
+++ public/js/socket.js
@@ -16,50 +16,55 @@
 
   console.log('socket', socket.socket)
 
-  socket.on('connect', function() {
-    root.playerId = socket.socket.sessionid
-
-
-    socket.on('attack', function(str, id) {
-      console.log('server attack')
-
-      root.usedWords[str] = id
-      root.exports.incomingWord(str, (id === root.playerId))
-    })
-
-    socket.on('destroy', function(str, id) {
-      console.log('server block')
-      
-      root.exports.destroyWord(str, (id === root.playerId))
-    })
-  })
-
-  // Blocking
-  // --------
-
-
-  // Attacking
-  // ---------
-
-  function attack(str, fn) {
-    console.log('attack', str)
-    socket.emit('attack', str, function(err) {
-      if (err) {
-        console.log('Attack Error: ', err)
-      }
-      fn && fn(err)
-    })
-  }
-
-  // Exports
-  // -------
-
-  root.attack = attack
-
-  // Testing
   $(function() {
-    
-    
+
+    var usedWords = $('#used-words')
+
+
+    socket.on('connect', function() {
+      root.playerId = socket.socket.sessionid
+
+
+      socket.on('attack', function(str, id) {
+        console.log('server attack')
+
+        root.usedWords[str] = id
+        usedWords.prepend('<li>'+str+'</li>')
+        root.exports.incomingWord(str, (id === root.playerId))
+      })
+
+      socket.on('destroy', function(str, id) {
+        console.log('server block')
+        
+        root.exports.destroyWord(str, (id === root.playerId))
+      })
+    })
+
+    // Blocking
+    // --------
+
+
+    // Attacking
+    // ---------
+
+    function attack(str, fn) {
+      console.log('attack', str)
+      socket.emit('attack', str, function(err) {
+        if (err) {
+          console.log('Attack Error: ', err)
+        }
+        fn && fn(err)
+      })
+    }
+
+    // Exports
+    // -------
+
+    root.attack = attack
+
+    // Testing
+      
+      
   })
 
 })()
